9b1cb7f4fb3a387f07d8e29ad8eec09b38811007
Bryan`Y. Limz <superuser@brylimz.com>
2021-11-30 15:48:54 +0700

    logics added

--- main.py
+++ main.py
@@ -10,3 +10,53 @@ print(name)
 
 random.shuffle(game_data.dataz)
 print(game_data.dataz)
+# higher
+# lower..
+# compare
+# variable
+# 's value from the list.
+#
+# Welcome
+# to
+# the
+# higher
+# lower
+# game!
+#
+# compare
+# A(print):
+# import
+# from list
+#
+# [random / shuffle][name][profession][location]
+#
+# Against
+# B(print):
+# import
+# from list
+#
+# [random / shuffle][name][proffesion][location]
+#
+# Who
+# has
+# more
+# follower ? Type
+# "A" or "B": (Print)
+#
+# not_end_game = true
+# while not_end_game = true
+# if compare A (Follower).list > compare B (Follower).list:
+#     you
+#     get
+#     the
+#     point.
+#     next
+#     question(
+#     for activated).
+#     else:
+#         not_end_game = false
+#     you
+#     wrong, you
+#     lose(print).
+#
+
