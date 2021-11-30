3dfc0c1a83021dba8b05848657a7ab9f4e13e2e1
Bryan`Y. Limz <superuser@brylimz.com>
2021-11-30 20:41:43 +0700

    score added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 from game_data import data
 from art import logo, vs
 print(art.logo)
-
+score = 0
 
 def format_data(account):
     """ Format the account data and returns into printable format."""
@@ -16,7 +16,10 @@ def format_data(account):
 
 def check_answer(guess, a_followers, b_followers):
     """ Take the user guess and follower counts and returns if they got it right"""
-
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
 
 
 # generate a random account from the game data
@@ -32,12 +35,18 @@ print(f"Against B: {format_data(account_b)}")
 guess = input("Who has more followers?, Type 'A' or 'B'").lower()
 
 # check if user is correct
+# # get follower count of each account
 a_follower_count = account_a["follower_count"]
 b_follower_count = account_b["follower_count"]
-
-# # get follower count of each account
 # # use if statement to check if user is correct
+is_correct = check_answer(guess, a_follower_count,b_follower_count)
 # give user feedback on their guess
+if is_correct:
+    score += 1
+    print(f"You're right! Current score: {score}.")
+else:
+    print(f"Sorry, that's wrong. Final score: {score}")
+
 # score keeping
 # make the game repeatable
 # making account at position B become the next account at position A
